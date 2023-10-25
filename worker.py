25a3bd75b063577825ea07f24cd14a27db403df6
Alex <temper8@gmail.com>
2023-10-26 02:49:52 +0300

    сделал сортировку по 'ФИО ребенка'

--- worker.py
+++ worker.py
@@ -4,8 +4,9 @@ import exel
 def executor(fn):
     miac_df, svmed_df, mes_df = exel.read(fn)
  
-    print(mes_df.columns.values.tolist())
-
+  
+    print('-----------------')
+  
     lost_table = []
     svmed_mes = []
     for i, row in svmed_df.iterrows():
@@ -20,27 +21,27 @@ def executor(fn):
             #print(row)
 
     svmed_mes_df = pd.DataFrame(svmed_mes)
-    print('-----------------')
+
     #print(pd.DataFrame(lost_table))
     print('-----------------')
 
     df = miac_df.join(svmed_mes_df.set_index('ФИО ребенка'), lsuffix='_миац', rsuffix='_св-мед', on='ФИО ребенка', how='outer', validate='m:1')
     col_list = df.columns.values.tolist()
-    print(col_list)
+    #print(col_list)
     first_list = ['ФИО ребенка', 'База_миац', 'База_св-мед', 'Врач', 'ФИО врача', 'Возрастная группа']
-    print(first_list)
+    #print(first_list)
     tail =  [item for item in col_list if item not in set(first_list)]
 
-    print(tail)
+    #print(tail)
 
     out_df = df[first_list+tail]
     #print(out_df)
 
     res = {
-        'Сводка'     : out_df,
-        'МИАЦ'       : miac_df,
-        'СВ-МЕД'     : svmed_df,
-        'СВ-МЕД МЭС' : mes_df
+        'Сводка'     : out_df.sort_values(by=['ФИО ребенка']),
+        'МИАЦ'       : miac_df.sort_values(by=['ФИО ребенка']),
+        'СВ-МЕД'     : svmed_df.sort_values(by=['ФИО ребенка']),
+        'СВ-МЕД МЭС' : mes_df.sort_values(by=['ФИО ребенка'])
     }
 
     exel.write_results(res)
